05dcd8b8b9403d4239c1f493117e75fa54de5ad5
nshadov <nshadov.eq@gmail.com>
2024-05-22 23:08:10 +0200

    i2c motor test

--- lolin_i2c_motor.py
+++ lolin_i2c_motor.py
@@ -1,12 +1,12 @@
 # Built based on https://github.com/wemos/LOLIN_I2C_MOTOR_Library/blob/master/src/LOLIN_I2C_MOTOR.cpp
 
-MOTOR_CH_A = b'0'
-MOTOR_CH_B = b'1'
-MOTOR_CH_BOTH = b'2'
+MOTOR_CH_A = 0x00
+MOTOR_CH_B = 0x01
+MOTOR_CH_BOTH = 0x02
 
-MOTOR_STATUS_STOP = b'0'
-MOTOR_STATUS_CCW = b'1'
-MOTOR_STATUS_CW = b'2'
+MOTOR_STATUS_STOP = 0x00
+MOTOR_STATUS_CCW = 0x01
+MOTOR_STATUS_CW = 0x02
 
 '''
   motor.changeFreq(MOTOR_CH_BOTH, 1000); //Change A & B 's Frequency to 1000Hz.
@@ -31,20 +31,24 @@ class Lolin_I2C_Motor:
         data = bytearray([0x05, channel])
         data.extend(freq.to_bytes(3, 'little'))
         self._i2c.writeto(self._address, data)
+        time.sleep_ms(50)
     
     def change_duty(self, channel, duty):
         data = bytearray([0x06, channel])
         duty_int = int(duty * 100)
         data.extend(duty_int.to_bytes(2, 'little'))
         self._i2c.writeto(self._address, data)
+        time.sleep_ms(50)
     
     def change_status(self, channel, status):
         data = bytearray([0x04, channel, status])
         self._i2c.writeto(self._address, data)
+        time.sleep_ms(50)
     
     def get_status(self):
         data = bytearray([0x01])
         self._i2c.writeto(self._address, data)
+        time.sleep_ms(50)
         data = self._i2c.readfrom(self._address, 2)
         product_id = data[0]
         version = data[1]
@@ -53,10 +57,13 @@ class Lolin_I2C_Motor:
     def reset(self):
         data = bytearray([0x02])
         self._i2c.writeto(self._address, data)
+        time.sleep_ms(50)
+
 
     def change_address(self, address):
         data = bytearray([0x03, address])
         self._i2c.writeto(self._address, data)
+        time.sleep_ms(50)
         
     def __str__(self):
-        return "{'i2c':"+str(self.id)+"}"
+        return "{'i2c':"+str(self._address)+"}"
