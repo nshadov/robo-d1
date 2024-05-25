1f3f61a279ba30e8d8634df987e987e5a10f6232
nshadov <nshadov.eq@gmail.com>
2024-05-25 14:37:12 +0200

    fix for random in older micropython versions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from machine import Pin
 from machine import I2C
-from random import randint
+from random import getrandbits
 import lolin_i2c_motor as lm
 
 
@@ -9,6 +9,15 @@ led = Pin(2, Pin.OUT)
 sclPin = Pin(5, Pin.OUT)
 sdaPin = Pin(4, Pin.OUT)
 I2C_MOTOR_ID = 48
+
+def random():
+    r = int(getrandbits(8)) * 4
+    if r > 1000:
+        print("R="+str(100))
+        return 100
+    else:
+        print("R="+str(int(r/10)))
+        return int(r/10)
 
 class Main:
     def __init__(self):
@@ -31,10 +40,10 @@ class Main:
         while True:
             if led.value():
                 led.off()
-                self.motor.change_duty(lm.MOTOR_CH_A, randint(0, 100));
-                self.motor.change_duty(lm.MOTOR_CH_B, randint(0, 100));
+                self.motor.change_duty(lm.MOTOR_CH_A, int(20+random()));
+                self.motor.change_duty(lm.MOTOR_CH_B, int(20+random()));
             else:
                 led.on()
-                self.motor.change_duty(lm.MOTOR_CH_A, randint(0, 100));
-                self.motor.change_duty(lm.MOTOR_CH_B, randint(0, 100));
-            time.sleep_ms(500 + randint(0, 500))
+                self.motor.change_duty(lm.MOTOR_CH_A, int(20+random()));
+                self.motor.change_duty(lm.MOTOR_CH_B, int(20+random()));
+            time.sleep_ms(250 + int(random()*10))
